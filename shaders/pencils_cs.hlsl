bfc328c9ff4189ca6b682a50003f854ff23309cc
Grisha Szep <grisha.szep@gmail.com>
2024-06-22 13:59:57 +0100

    first attempts at pencil shader

--- shaders/pencils_cs.hlsl
+++ shaders/pencils_cs.hlsl
@@ -1,20 +1,34 @@
 cbuffer variables : register(b0)
 {
+	uint width;
+	uint height;
 	float delta;
 };
 
-struct pencil {
+struct pencil
+{
 	float2 position;
 	float2 velocity;
 };
 
 RWStructuredBuffer<pencil> pencils : register(u0);
+RWTexture2D<float4> traces : register(u1);
 
-[numthreads(1, 1, 1)]
-void main( uint3 id : SV_DispatchThreadID )
+[numthreads(16, 1, 1)] void main(uint3 id : SV_DispatchThreadID)
 {
 	float2 position = pencils[id.x].position;
 	float2 velocity = pencils[id.x].velocity;
-	
-	pencils[id.x].position += velocity*delta;
+
+	position += velocity * delta;
+
+	// Bounce off the walls
+	if (position.y < 0 || position.y > height)
+	{
+		velocity.y *= -1.0;
+		//position.y = clamp(position.y, 0, height);
+	}
+
+	pencils[id.x].position = position;
+	pencils[id.x].velocity = velocity;
+	traces[position] = float4(1, 1, 1, 1);
 }
--- shaders/postprocessing_ps.hlsl
+++ shaders/postprocessing_ps.hlsl
@@ -14,7 +14,6 @@ struct render_targets
 	float4 viewport : SV_TARGET0;
 };
 
-
 render_targets main(screen input)
 {
 	render_targets output = (render_targets) 0;
