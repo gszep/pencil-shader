786d37ca6ab1444c3f06bc010e9728e762a1074a
Grisha Szep <grisha.szep@gmail.com>
2024-06-22 11:07:44 +0100

    detect edges between background and object

--- shaders/edges_ps.hlsl
+++ shaders/edges_ps.hlsl
@@ -1,6 +1,8 @@
 cbuffer variables : register(b0)
 {
 	float2 viewport_size;
+	float thickness;
+	float threshold;
 };
 
 struct screen
@@ -17,16 +19,29 @@ struct render_targets
 	float4 edges : SV_TARGET0;
 };
 
-static const float2 delta = 5.0 / viewport_size;
+static const float2 delta = thickness / viewport_size;
 static const float2 du = float2(delta.x, 0);
 static const float2 dv = float2(0, delta.y);
 
 float is_edge(float2 uv)
 {
-	float edge_u = dot(normals.Sample(state, uv + du).xyz, normals.Sample(state, uv - du).xyz);
-	float edge_v = dot(normals.Sample(state, uv + dv).xyz, normals.Sample(state, uv - dv).xyz);
 
-	return edge_u < 0.5 || edge_v < 0.5;
+	float4 du1 = normals.Sample(state, uv + du);
+	float4 du2 = normals.Sample(state, uv - du);
+
+	float4 dv1 = normals.Sample(state, uv + dv);
+	float4 dv2 = normals.Sample(state, uv - dv);
+
+	du1.a = 1;
+	du2.a = 1;
+
+	dv1.a = 1;
+	dv2.a = 1;
+
+	float edge_u = dot(normalize(du1), normalize(du2));
+	float edge_v = dot(normalize(dv1), normalize(dv2));
+
+	return edge_u < threshold || edge_v < threshold;
 }
 
 render_targets main(screen input)
