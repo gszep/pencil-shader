875a65d712ef8180c289fcaf1feb93bf3cd6be1c
Grisha Szep <grisha.szep@gmail.com>
2024-06-26 23:02:30 +0100

    rotate geometry

--- shaders/gbuffer_vs.hlsl
+++ shaders/gbuffer_vs.hlsl
@@ -23,8 +23,14 @@ gbuffer main(vertex input)
 {
 	gbuffer output = (gbuffer)0;
 
-	float4 world_position = geometry_transform * float4(input.position, 1.0);
-	output.normal = geometry_transform * float4(input.normal, 0.0);
+	float4x4 rotate = float4x4(
+		cos(time), 0, sin(time), 0,
+		0, 1, 0, 0,
+		-sin(time), 0, cos(time), 0,
+		0, 0, 0, 1);
+
+	float4 world_position = rotate * geometry_transform * float4(input.position, 1.0);
+	output.normal = rotate * geometry_transform * float4(input.normal, 0.0);
 
 	float4 view_position = view * world_position;
 	output.position = projection * view_position;
