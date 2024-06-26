f42f53e9709890237e73ae90906577a96499e200
Grisha Szep <grisha.szep@gmail.com>
2024-06-26 23:16:19 +0100

    formatting

--- shaders/postprocessing_ps.hlsl
+++ shaders/postprocessing_ps.hlsl
@@ -17,13 +17,13 @@ struct render_targets
 
 render_targets main(screen input)
 {
-	render_targets output = (render_targets) 0;
+	render_targets output = (render_targets)0;
 	input.uv.y = 1 - input.uv.y;
 
-	output.viewport += traces.Sample(state,input.uv);
-	//output.viewport += normals.Sample(state,input.uv);
-	//output.viewport += depth.Sample(state,input.uv);
-	//output.viewport += edges.Sample(state,input.uv);
-	
+	output.viewport += traces.Sample(state, input.uv);
+	// output.viewport += normals.Sample(state, input.uv);
+	// output.viewport += depth.Sample(state, input.uv);
+	// output.viewport += edges.Sample(state, input.uv);
+
 	return output;
 }
